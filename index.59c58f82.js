572b9bea12b8e6de5c7f5b3811ba798d8721a21c
statsenkoin <34393845+statsenkoin@users.noreply.github.com>
2022-10-09 13:12:13 +0000

    Deploying to gh-pages from @ statsenkoin/ice-cream-beta@a256b44edffb683e702b7420037072c9dc92a0a1 🚀

--- index.59c58f82.js
+++
@@ -1,2 +0,0 @@
-new Swiper(".swiper",{direction:"horizontal",loop:!0,pagination:{el:".swiper-pagination",type:"bullets",clickable:!0},speed:500,spaceBetween:10});
-//# sourceMappingURL=index.59c58f82.js.map
