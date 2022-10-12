7323d621a73a13f66e5046f727a205d014d0c211
Vitaliy Lyalin <vitaliy.lyalin@gmail.com>
2022-10-12 09:42:39 +0300

    Додає зміни розміра фону, приховані заголовки секції customer-reviews та gallery

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,9 +1,7 @@
 const swiper = new Swiper('.swiper', {
-  // Optional parameters
   direction: 'horizontal',
   loop: true,
 
-  // If we need pagination
   pagination: {
     el: '.swiper-pagination',
     type: 'bullets',
