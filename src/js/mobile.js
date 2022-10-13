1bb4d429ee17374e31415ccc7dc78de508586c93
Ihor Statsenko <statsenkoin@gmail.com>
2022-10-13 07:57:28 +0300

    mobile menu links + scroll

--- src/js/mobile.js
+++ src/js/mobile.js
@@ -2,11 +2,19 @@
   const refs = {
     openMenuBtn: document.querySelector('[data-menu-open]'),
     closeMenuBtn: document.querySelector('[data-menu-close]'),
+    homeMenuBtn: document.querySelector('[data-menu-home]'),
+    aboutMenuBtn: document.querySelector('[data-menu-about]'),
+    productsMenuBtn: document.querySelector('[data-menu-products]'),
+    contactsMenuBtn: document.querySelector('[data-menu-contacts]'),
     menu: document.querySelector('[data-menu]'),
   };
 
   refs.openMenuBtn.addEventListener('click', toggleModal);
   refs.closeMenuBtn.addEventListener('click', toggleModal);
+  refs.homeMenuBtn.addEventListener('click', toggleModal);
+  refs.aboutMenuBtn.addEventListener('click', toggleModal);
+  refs.productsMenuBtn.addEventListener('click', toggleModal);
+  refs.contactsMenuBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
     refs.menu.classList.toggle('is-open');
