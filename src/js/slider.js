ad1ac2b8c51b630d8c6ebed9b7f6fd407096fc72
Vitaliy Lyalin <vitaliy.lyalin@gmail.com>
2022-10-16 16:41:11 +0300

    Змінює на один js файл для сладерів customer-reviews та  gallery
    Додатково змінює в слайдері gallery перемикання на effect: 'fade' стало  візуально більш привабливою

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,16 +1,41 @@
-const swiper = new Swiper('.swiper', {
+var swiper1 = new Swiper('.swiper1', {
   direction: 'horizontal',
   loop: true,
+
+  slidesPerView: 1,
+
+  effect: 'fade',
+  fadeEffect: { crossFade: true },
+
+  autoHeight: true,
+
+  autoplay: {
+    delay: 1500,
+    disableOnInteraction: false,
+  },
+});
+
+var swiper2 = new Swiper('.swiper2', {
+  direction: 'horizontal',
+  loop: true,
+
+  slidesPerView: 1,
 
   pagination: {
     el: '.swiper-pagination',
     type: 'bullets',
     clickable: true,
   },
+
+  keyboard: {
+    anabled: true,
+    onlyInViewport: true,
+    pageUpDown: true,
+  },
+
   speed: 500,
 
   effect: 'flip',
-
   flipEffect: {
     sladeShadows: true,
     limitRotation: true,
